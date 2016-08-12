b6dcaaa079d50af777ddb41c04c3d508023a7cc2
bishorup.banjara@outlook.com <[bishorup.banjara@gmail.com]>
2016-08-11 23:34:56 -0400

    Complete Router and Cleanups

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Recipe } from '../recipe';
 import { RecipeItemComponent } from  './recipe-item.component';
@@ -13,16 +13,11 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [];
-  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onSelected(recipe: Recipe) {
-    this.recipeSelected.emit(recipe);
   }
 
 }
