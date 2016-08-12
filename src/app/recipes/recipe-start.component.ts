f6f0673a6c707b44de31aab6df2817d40c333171
bishorup.banjara@outlook.com <[bishorup.banjara@gmail.com]>
2016-08-11 23:26:44 -0400

    Fixed Route Issues

--- src/app/recipes/recipe-start.component.ts
+++ src/app/recipes/recipe-start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -10,11 +10,8 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: []
 })
-export class RecipeStartComponent implements OnInit {
+export class RecipeStartComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- src/app/recipes/recipes.routes.ts
+++ src/app/recipes/recipes.routes.ts
@@ -1,10 +1,11 @@
 import { RouterConfig } from '@angular/router';
 import { RecipeStartComponent } from './recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 export const RECIPE_ROUTES: RouterConfig = [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeStartComponent },
+    { path: ':id', component: RecipeDetailsComponent },
     { path: ':id/edit', component: RecipeEditComponent }
 ];
